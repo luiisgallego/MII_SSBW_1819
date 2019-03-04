579f0386d38dec3a2bc5c165fe1fe24df0bead4a
Luis Gallego Quero <lgaq94@gmail.com>
2019-03-04 18:16:47 +0100

    comienzo ejercicio 1

--- ejercicios/urls.py
+++ ejercicios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# path('hola_mundo', views.hola_mundo),	# entrada str
-    path('1/<str:usuario>', views.ejercicio_1),	# entrada str
+	path('0_1', views.hola_mundo),	
+    path('0_2/<str:usuario>', views.hola_mundo_nombre),	
+	path('1', views.ejercicio1),
 ]
--- ejercicios/views.py
+++ ejercicios/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
-
 def hola_mundo(request):
     salida = '''<html>Hola mundo</html>'''
     return HttpResponse(salida)
 
-
-def ejercicio_1(request, usuario):
+def hola_mundo_nombre(request, usuario):
     salida = '''<html>Hola mundo %s</html>''' % (usuario)
     return HttpResponse(salida)
+
+def ejercicio1(request):
+    #salida = '''<html>Hola mundo ejercicio1</html>'''
+    lista = ['hola', 'adios', 'yes']
+    aux = 0
+
+    for valor in lista:
+        if len(valor) > 1:
+            aux += 1
+    
+    return HttpResponse(aux)
